22d8f608a318e768cfe564e91b31b83d1fe36c0c
Aditya <jhaadi4444@gmail.com>
2025-07-23 17:02:20 +0530

    django filters and using base templates

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from app.views import home, AcrticleCreateView
+from app.views import AcrticleCreateView, ArticleListView, AcrticleUpdateView, AcrticleDeleteView
 
 urlpatterns = [
-    path('',  home , name='home'),
-    path('articles/create/', AcrticleCreateView.as_view(), name='create_article'),
+    path('',  ArticleListView.as_view() , name='home'),
+    path('create/', AcrticleCreateView.as_view(), name='create_article'),
+    path('<int:pk>/update', AcrticleUpdateView.as_view(), name='update_article'),
+    path('<int:pk>/delete', AcrticleDeleteView.as_view(), name='delete_article'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from app.models import ArticleData
-from django.views.generic import CreateView
+from django.views.generic import CreateView,ListView,UpdateView,DeleteView
 from django.urls import reverse_lazy
 
 
-def home(request):
-    article = ArticleData.objects.all()
-    return render(request , 'app/home.html' , {"article":article})
+class ArticleListView(ListView):
+    model = ArticleData
+    template_name = 'app/home.html'
+    context_object_name = 'articles'
 
 class AcrticleCreateView(CreateView):
     model = ArticleData
     fields = ['title', 'content', 'word_counts', 'twitter_post', 'status']
     template_name = 'app/article_create.html'
     success_url = reverse_lazy('home')
+    
+class AcrticleUpdateView(UpdateView):
+    model = ArticleData
+    fields = ['title', 'content', 'word_counts', 'twitter_post', 'status']
+    template_name = 'app/article_update.html'
+    success_url = reverse_lazy('home')
+    context_object_name = "article"
+    
+class AcrticleDeleteView(DeleteView):
+    model = ArticleData
+    template_name = 'app/article_delete.html'
+    success_url = reverse_lazy('home')
+    context_object_name = "article"
+
