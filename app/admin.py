ffd524cee613d4ac151c1d70560568ea35b869a0
Aditya <jhaadi4444@gmail.com>
2025-07-24 14:57:57 +0530

    admin panel view changes

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from app.models import UserData, ArticleData
 
-
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'word_counts', 'status', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('status', )
+    date_hierarchy = 'created_at'
+    ordering = ('created_at',)
+    readonly_fields = ('word_counts', 'created_at', 'updated_at')
+    
 admin.site.register(UserData)
-admin.site.register(ArticleData)
+admin.site.register(ArticleData, ArticleAdmin)
